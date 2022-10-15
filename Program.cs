7ad01a20a9592c6f69f19ee11a0c065520534481
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-15 19:50:45 +0530

    check for power of 2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace poweroftwo
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int N = 0;
+            while (N <= 10)
+            {
+                var Power = Math.Pow(2, N);
+                N++;
+                Console.WriteLine(Power);
+            }
+        }
+    }
+}
